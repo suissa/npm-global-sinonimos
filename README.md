77eee5c26cd3f61241740527f788bd888ed34ddc
Suissa <jnascimento@gmail.com>
2016-12-09 15:59:23 -0200

    Foiii

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sinonimo
+
+Módulo global para consultar palavras sinônimos via terminal.
+
+Para instalar basta:
+
+```
+npm i -g sinonimo
+```
+
+Para executar, no seu terminal, faça:
+
+```
+sinonimo palavra
+```
--- antonimo.js
+++ antonimo.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio')
 const utf8 = require('utf8')
 const words = process.argv
 const word = process.argv[process.argv.length-1]
-const url = 'http://www.sinonimos.com.br/'
+const url = 'http://www.antonimos.com.br/'
 
 const consult = `${url}${word}/`
 let body = ''
@@ -16,10 +16,10 @@ http.get(consult, (res) =>
     .on('data', data => body += data)     
     .on('end', () => {
       let $ = cheerio.load(body)
-      const sentido = $('.sentido').text()
-      const sinonimos = $('.sinonimos').text().split('.')
+      // const sentido = $('.sentido').text()
+      const antonimos = $('.antonimos').text().split('.')
       // console.log('data', body            )
-      console.log('sentido', sentido)
-      console.log('sinonimos', sinonimos)
+      // console.log('sentido', sentido)
+      console.log('antonimos', antonimos)
     })
 )
